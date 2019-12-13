78834c1b0b65a078db56cab947f8ca46c095088d
Ahmad Bagus <50206121+ahmadbagus99@users.noreply.github.com>
2019-12-13 17:03:49 +0700

    123

--- src/app/members/dashboard/dashboard.page.ts
+++ src/app/members/dashboard/dashboard.page.ts
@@ -23,32 +23,4 @@ export class DashboardPage implements OnInit {
  
   ngOnInit() {
   }
-  ionViewWillEnter() {
-    this.itemTotalProspect = [];
-    this.LoadTotalProspect();
-    
-  }
-
-  LoadTotalProspect() {
-    this.storage.get('session_storage').then((iduser) => {
-      var userID = iduser;
-      var newID = userID.map(user => user.id);
-      this.userID = parseInt(newID)
-      let body = {
-        aksi: 'getdata',
-        limit: this.limit,
-        start: this.start,
-      };
-      this.postPvdr.postData(body, 'LoadTotalProspect.php?Id='+this.userID).subscribe(data => {
-        for (let item of data) {
-          this.itemTotalProspect.push(item);
-          this.totalProspect = this.itemTotalProspect.length;
-        }
-      });
-    })
-  }
- //fungsi sebagai buttin keluar applikasi/keluar akun
-  logout() {
-    this.authService.logout();
-  }
 }
